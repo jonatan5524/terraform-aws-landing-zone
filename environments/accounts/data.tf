033ded73fd174de277545c65976d078abf966c37
jonatan <jonatan5524@gmail.com>
2026-05-17 20:27:29 +0300

    feat: add identity store group lookup for developers

--- environments/accounts/data.tf
+++ environments/accounts/data.tf
@@ -7,3 +7,16 @@ data "terraform_remote_state" "management" {
     region = "us-east-1"
   }
 }
+
+data "aws_ssoadmin_instances" "this" {}
+
+data "aws_identitystore_group" "developers" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.this.identity_store_ids)[0]
+
+  alternate_identifier {
+    unique_attribute {
+      attribute_path  = "DisplayName"
+      attribute_value = "developers"
+    }
+  }
+}
